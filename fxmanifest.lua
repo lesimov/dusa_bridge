af58f9cc0b622f2858c040e23c17873a9b361318
mert <81008147+lesimov@users.noreply.github.com>
2024-11-13 01:40:18 +0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+
+game 'gta5'
+
+author 'Dusa'
+version '0.6.1'
+description 'Dusa Bridge'
+
+shared_script 'override.lua'
+shared_script {
+    '@ox_lib/init.lua',
+}
+
+files {
+    '**/**/client.lua',
+    '**/*.lua',
+}
+
+escrow_ignore {
+    'bridge.lua',
+    'override.lua',
+    '**/*.lua',
+}
+
+dependency '/assetpacks'
+dependency '/assetpacks'
+dependency '/assetpacks'
