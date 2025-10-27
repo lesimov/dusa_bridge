26bacd6700623c6e99d86afbec46733cee1ecf63
Austin Dunn <austindunn.dev@gmail.com>
2025-10-27 16:45:04 -0600

    properly attribute credit and license to sleepless_interact

--- interaction/web/js/main.js
+++ interaction/web/js/main.js
@@ -1,3 +1,11 @@
+/*
+    https://github.com/Sleepless-Development/sleepless_interact
+
+    This file is licensed under LGPL-3.0 or higher <https://www.gnu.org/licenses/lgpl-3.0.en.html>
+
+    Copyright © 2025 DemiAutomatic <https://github.com/DemiAutomatic>
+*/
+
 import { createOptions } from "./createOptions.js";
 import { fetchNui } from "./fetchNui.js";
 import { onSelect } from "./controls.js";
